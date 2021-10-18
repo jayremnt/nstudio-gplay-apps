4707d434ed0656f0d0f8a7d9a821015bb21f1357
jayremnt <jayremnt@gmail.com>
2021-10-18 16:36:51 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const gplayHandler = require('google-play-scraper');
+const fs = require('fs');
 
-gplayHandler.developer({devId: 'NorthStudio'}).then(console.log);
+gplayHandler.developer({devId: 'NorthStudio'}).then(data => {
+  data = JSON.stringify(data);
+  console.log(data);
+  fs.writeFile('nstudio.json', data, (e) => {
+    if (e) {
+      console.log(e);
+    }
+    console.log("saved");
+  });
+});
+
